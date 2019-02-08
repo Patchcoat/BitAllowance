93902d843a5689a7617844984a96ceb6c3105227
Patchcoat <g2175410@nwytg.com>
2019-02-07 17:27:20 -0800

    added test activity

---
+++ app/src/main/java/com/bitallowance/RemoteDatabaseTest.java
@@ -0,0 +1,13 @@
+package com.bitallowance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteDatabaseTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_database_test);
+    }
+}
