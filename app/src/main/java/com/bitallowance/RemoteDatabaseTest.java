6810aafb8d879754473b254a33db0db119acfe05
Patchcoat <g2175410@nwytg.com>
2019-02-19 23:23:30 -0800

    improved test activity and tested it with server.

--- app/src/main/java/com/bitallowance/RemoteDatabaseTest.java
+++ app/src/main/java/com/bitallowance/RemoteDatabaseTest.java
@@ -6,65 +6,40 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
 public class RemoteDatabaseTest extends AppCompatActivity {
 
     private String tag = "Network Socket";
-    private Socket socket = null;
-    private DataInputStream input = null;
-    private DataOutputStream out = null;
-    private static final int SERVERPORT = 3490;
+    private Socket clientSocket = null;
+    private BufferedReader input = null;
+    private OutputStream out = null;
+    private static final int SERVER_PORT = 3490;
     private static final String SERVER_IP = "107.174.13.151";
 
     public void Connect(View view) {
-        try {
-            input = new DataInputStream(System.in);
-            out = new DataOutputStream(socket.getOutputStream());
-        } catch (UnknownHostException u) {
-            Log.d(tag, u.toString());
-        } catch (IOException i) {
-            Log.d(tag, i.toString());
-        }
-
-        String line = "";
-        if (input == null) {
-            TextView textView = findViewById(R.id.textView);
-            textView.setText("Error connecting");
-            return;
-        }
-        while (!line.equals("Over")) {
-            try {
-                line = input.readLine();
-                out.writeUTF(line);
-            } catch (IOException i) {
-                Log.d(tag, i.toString());
-            }
-        }
-
-        try {
-            input.close();
-            out.close();
-            socket.close();
-        } catch (IOException i) {
-            Log.d(tag, i.toString());
-        }
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(line);
+        new Thread(new ClientThread()).start();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_database_test);
-
-        new Thread(new ClientThread()).start();
     }
 
     class ClientThread implements Runnable {
@@ -72,11 +47,38 @@ public class RemoteDatabaseTest extends AppCompatActivity {
         public void run() {
             try {
                 InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-                socket = new Socket(serverAddr, SERVERPORT);
+                SocketAddress sockaddr = new InetSocketAddress(serverAddr, SERVER_PORT);
+                clientSocket = new Socket();
+
+                int timeout = 2000;
+                clientSocket.connect(sockaddr, timeout);
             } catch (UnknownHostException el) {
                 el.printStackTrace();
             } catch (IOException el) {
                 el.printStackTrace();
+            }
+
+            try {
+                String string = "Hello There!";
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                writer.write(string);
+                writer.flush();
+                Log.d(tag, "Sent \"Hello There!\"");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String str;
+                Log.d(tag, "Received,");
+                while ((str = reader.readLine()) != null) {
+                    Log.d(tag, str);
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
