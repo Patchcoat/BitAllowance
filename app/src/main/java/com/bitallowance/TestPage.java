61463b476a2b4003ae6f835f79fb7e5e8c689364
Doug Barlow <bar13010@byui.edu>
2019-03-04 09:21:47 -0800

    Added a TestPage activity to contain links to the various pages we're testing. Link to test page is off the main activity. This way we can simply delete the test page activity when it comes time to launch the app and it won't crowd the main activity.

---
+++ app/src/main/java/com/bitallowance/TestPage.java
@@ -0,0 +1,18 @@
+package com.bitallowance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestPage extends AppCompatActivity {
+
+    /**
+     * This class is meant to link to Activities we need to test
+     * @param
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_page);
+    }
+}
