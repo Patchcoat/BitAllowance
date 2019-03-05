618da1fc5ec09a68bac1058b9b26d2f8cc2e101d
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-03-05 12:15:05 -0800

    ReserveHome with test button, Async Method

--- app/src/main/java/com/bitallowance/ReserveHome.java
+++ app/src/main/java/com/bitallowance/ReserveHome.java
@@ -1,15 +1,21 @@
 package com.bitallowance;
 
-import android.database.DataSetObserver;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ReserveHome extends AppCompatActivity {
 
@@ -79,5 +85,93 @@ public class ReserveHome extends AppCompatActivity {
 
     public void openGiveReward(View view) {
 
+    }
+
+    public void onClickCreate(View view) {
+        Context context = this.getApplicationContext();
+        MyTask myTask = new MyTask();
+        myTask.execute(true);
+    }
+
+    private class MyTask extends AsyncTask<Boolean, Integer, ArrayList> {
+
+        @Override
+        protected ArrayList doInBackground(Boolean... booleans) {
+
+            if (booleans[0])
+                saveFile();
+            else
+                loadFile();
+
+            return null;
+        }
+
+        private void saveFile()
+        {
+            FileOutputStream outStream;
+            Random random = new Random();
+            String fileContents = "";
+            int value;
+            try{
+                outStream = openFileOutput("testFile.txt", Context.MODE_PRIVATE);
+                for (int i = 1; i <= 10; i++)
+                {
+                    value = random.nextInt(1900000000);
+                    fileContents += value + "\n";
+                    publishProgress(i, value, 1);
+                    Thread.sleep(250);
+                }
+                outStream.write(fileContents.getBytes());
+                outStream.close();
+            }
+            catch (Exception e)
+            {
+                Log.d("DMB: saveFile()", "File save failed!");
+            }
+
+        }
+
+        private void loadFile()
+        {
+            FileInputStream inStream;
+            int value;
+            try{
+                inStream = openFileInput("testFile.txt");
+                InputStreamReader reader = new InputStreamReader(inStream);
+                BufferedReader buffer = new BufferedReader(reader);
+                for (int i = 1; i <= 10; i++)
+                {
+                    value = Integer.parseInt(buffer.readLine());
+                    publishProgress(i, value, 0);
+                    Thread.sleep(250);
+                }
+                inStream.close();
+            }
+            catch (Exception e)
+            {
+                Log.d("DMB: loadFile()", "File load failed!");
+            }
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            if (values[2] == 1) {
+                entityList.add("Here is a list" + values[0]);
+                taskList.add("Just for fun" + values[1]);
+                rewardList.add("Creating Key " + values[0] + ": x00" + values[1]);
+            }
+            else {
+                rewardList.add("Loading Key " + values[0] + ": x00" + values[1]);
+            }
+
+            mEntityList.notifyDataSetChanged();
+            mTaskList.notifyDataSetChanged();
+            mRewardList.notifyDataSetChanged();
+
+            entityView.smoothScrollToPosition(entityList.size()-1);
+            taskView.smoothScrollToPosition(taskList.size()-1);
+            rewardView.smoothScrollToPosition(rewardList.size()-1);
+        }
     }
 }
