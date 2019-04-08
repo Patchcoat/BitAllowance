4664357bf7f71b1292674103b6e4c9764fe6ff29
Doug Barlow <bar13010@byui.edu>
2019-04-08 12:39:37 -0700

    Set to isPHP to true

--- app/src/main/java/com/bitallowance/Reserve.java
+++ app/src/main/java/com/bitallowance/Reserve.java
@@ -16,7 +16,7 @@ public class Reserve {
     private static List<Transaction> _transactionList = new ArrayList<>();
     private static String _currencyName;
     private static String _currencySymbol;
-    static boolean serverIsPHP = false;
+    static boolean serverIsPHP = true;
 
     public Reserve() {
     }
